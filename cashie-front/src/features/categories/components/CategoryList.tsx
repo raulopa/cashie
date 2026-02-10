357f62962e9c28abd26f0560807a1fa0ed6a2d39
Raúl López <raulopadev@gmail.com>
2026-02-10 16:53:22 +0100

    Primera comunicación exitosa: POST y GET contra api autenticada

--- cashie-front/src/features/categories/components/CategoryList.tsx
+++ cashie-front/src/features/categories/components/CategoryList.tsx
@@ -2,6 +2,9 @@ import { Button } from "@heroui/button";
 import { PlusIcon } from "lucide-react";
 import NewCategoryModal from "./NewCategoryModal";
 import { useDisclosure } from "@heroui/modal";
+import { CategoryService } from "../services/category-service";
+import { useEffect, useState } from "react";
+import CategoryDto from "../dtos/category-dto";
 
 interface CategoryListProps {
     className?: string;
@@ -9,6 +12,20 @@ interface CategoryListProps {
 
 export default function CategoryList({ className }: CategoryListProps) {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [categories, setCategories] = useState<CategoryDto[]>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const categories = await CategoryService.getAll();
+                setCategories(categories);
+            } catch (error) {
+                console.error("Error cargando categorías:", error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
 
     return (
         <div className={`w-full h-full bg-content1 rounded-2xl shadow-xl border border-divider overflow-hidden ${className}`}>
@@ -18,7 +35,11 @@ export default function CategoryList({ className }: CategoryListProps) {
                     <PlusIcon />
                 </Button>
             </div>
-
+            <div>
+                {categories.map((category: any) => (
+                    <p key={category.id}>{category.name}</p>
+                ))}
+            </div>
             <NewCategoryModal isOpen={isOpen} onOpenChange={onOpenChange} />
         </div>
     )
--- cashie-front/src/shared/api/api.ts
+++ cashie-front/src/shared/api/api.ts
@@ -20,7 +20,7 @@ api.interceptors.request.use(config => {
 api.interceptors.response.use(
     res => res,
     err => {
-        if (err.response?.status === 401) {
+        if (err.response?.status === 403) {
             useAuthStore.getState().logout();
             window.location.href = '/login';
         }
